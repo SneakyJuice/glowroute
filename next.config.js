61b4c41be52a99caef3a5763eb9192affca73270
SneakyJuice <anthonysealey@gmail.com>
2026-04-25 22:42:44 -0400

    fix: disable static export, use ssr/standalone output (avoid db timeouts during build)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   generateBuildId: async () => {
     return `build-${Date.now()}`
   },
   images: {
-    domains: ['maps.googleapis.com', 'lh3.googleusercontent.com'],
+    domains: ['maps.googleapis.com', 'lh3.googleusercontent.com', 'psiuknphchmhsthvhkpt.supabase.co'],
   },
   trailingSlash: false,
 }
